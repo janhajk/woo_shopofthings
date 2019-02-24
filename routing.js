ac0510b6cc0535efae87f012bf46a9a1204658c4
janhajk <janhajk@gmail.com>
2019-02-24 13:47:49 +0000

    label content

--- routing.js
+++ routing.js
@@ -32,13 +32,6 @@ var basic = function(app, connection) {
             //res.send(e ? e : data);
         });
     });
-    // app.get('/asset/:aid', auth.ensureAuthenticated, function(req, res) {
-    //     var aid = req.params.aid;
-    //     var assets = require(__dirname + '/lib/assets.js');
-    //     assets.get(aid, connection, function(e, data) {
-    //         res.send(e ? e : data);
-    //     });
-    // });
 
 };
 
