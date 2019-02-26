c87ce1c445bccf9fa86636919226a764439dcbba
janhajk <janhajk@gmail.com>
2019-02-26 16:07:04 +0000

    search all cells not just title

--- public/code.js
+++ public/code.js
@@ -383,14 +383,17 @@
                 table = tbody;
                 tr = table.getElementsByTagName("tr");
                 for (i = 0; i < tr.length; i++) {
-                    td = tr[i].getElementsByTagName("td")[1];
-                    if (td) {
-                        txtValue = td.textContent || td.innerText;
-                        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                            tr[i].style.display = "";
-                        }
-                        else {
-                            tr[i].style.display = "none";
+                    td = tr.getElementsByTagName('td');
+                    for (let s = 0; s < td.length; s++) {
+                        if (td[s]) {
+                            txtValue = td[s].textContent || td[s].innerText;
+                            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                                tr[i].style.display = "";
+                                break;
+                            }
+                            else {
+                                tr[i].style.display = "none";
+                            }
                         }
                     }
                 }
