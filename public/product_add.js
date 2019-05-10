dbaad87da70b6e1623228151c5d5a9633b1ea66b
janhajk <janhajk@gmail.com>
2019-05-10 15:32:42 +0000

    product add

---
+++ public/product_add.js
@@ -0,0 +1,45 @@
+/*global $ */
+(function() {
+      var add;
+
+      document.addEventListener('DOMContentLoaded', function() {
+            var dashline = document.getElementById('dashline');
+            add = new Add(dashline);
+      });
+
+      var Add = function(parent) {
+            var div = document.createElement('div');
+            div.style.width = 'auto';
+            div.style.float = 'right';
+            var button = document.createElement('button');
+            button.type = 'button';
+            button.className = 'btn btn-info';
+
+            var span = document.createElement('span');
+            span.className = 'glyphicon glyphicon-file';
+            button.appendChild(span);
+            div.appendChild(button);
+            parent.appendChild(div);
+
+            button.addEventListener('click', function() {
+                  let title = prompt('Title of the new Product');
+                  let snapshot = prompt('URL of the Purchase-Site');
+                  let params = 'title=' + title + '&snapshot=' + snapshot;
+                  let request = new XMLHttpRequest();
+                  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                  request.open('POST', '/ping', true);
+                  request.onload = function() {
+                        if (request.status >= 200 && request.status < 404) {
+                              alert('Product added, please reload site');
+                        }
+                  };
+                  request.onerror = function() {
+                        alert('Error when adding new Product');
+                  };
+                  request.send(params);
+
+            });
+
+      };
+
+})();
